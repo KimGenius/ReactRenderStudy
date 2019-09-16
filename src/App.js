fb1785e1ab87effd76e467ae9daffcd7444db8c4
GeniusK <geniusk1047@naver.com>
2019-09-16 15:37:52 +0900

    hook이랑 class 같이 나오게 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import ParentComponent from './components-class/ParentComponent'
-
+import ClassParentComponent from './components-class/ParentComponent'
+import HooksParentComponent from './components/ParentComponent'
 function App() {
-  return <ParentComponent />
+  return (
+    <div>
+      <div style={{ marginBottom: '3vh' }}>
+        Class
+        <ClassParentComponent />
+      </div>
+      <div style={{ marginBottom: '3vh' }}>
+        Hooks
+        <HooksParentComponent />
+      </div>
+    </div>
+  )
 }
 
 export default App;
